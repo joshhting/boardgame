b2258892754a1ac7b9565b42329691c44ddc5ea8
Josh Ting <joshting25@gmail.com>
2016-10-22 23:38:52 -0400

    Add tiled board

--- Card.java
+++ Card.java
@@ -8,16 +8,16 @@ public class Card {
     int cost;
     String text;
 
-    public Card(CardType type, int cost String text) {
+    public Card(CardType type, int cost, String text) {
         this.type = type;
         this.cost = cost;
         this.text = text;
     }
 
+    private enum CardType {
+        OPERATION,
+        SHIP,
+        ASSET
+    };
 }
 
-private enum CardType {
-    OPERATION,
-    SHIP,
-    ASSET
-};
